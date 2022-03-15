4dde5bbc5a53f0f7c0b6468550bc104637a7cc01
Isacc <dewrain331@gmail.com>
2022-03-15 17:33:45 +0900

    Award 파일 코드

--- front/src/components/award/Award.js
+++ front/src/components/award/Award.js
@@ -1,29 +1,27 @@
-import React, { useState } from "react";
-import AwardEditForm from "./AwardEditForm";
-import AwardCard from "./AwardCard";
-import * as Api from "../../api";
+import { useState } from "react"
+import AwardCard from "./AwardCard"
+import AwardEditForm from "./AwardEditForm"
 
-const Award = ({ award, setAwards, isEditable }) => {
-  // useState 훅을 통해 isEditing 상태를 생성함.
-  const [isEditing, setIsEditing] = useState(false);
-
+function Award({ award, setAwards, isEditable }) {
+  //useState로 isEditing 상태를 생성함.
+  const [isEditing, setIsEditing] = useState(false)
   return (
     <>
       {isEditing ? (
         <AwardEditForm
           currentAward={award}
-          setIsEditing={setIsEditing}
           setAwards={setAwards}
+          setIsEditing={setIsEditing}
         />
       ) : (
         <AwardCard
           award={award}
-          setIsEditing={setIsEditing}
           isEditable={isEditable}
+          setIsEditing={setIsEditing}
         />
       )}
     </>
-  );
+  )
 }
 
 export default Award
