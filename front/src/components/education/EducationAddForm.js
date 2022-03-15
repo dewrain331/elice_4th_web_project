26bd1de8d8531255b1e476b2c0db50e88f006372
bsy1141 <bsy1141@naver.com>
2022-03-15 16:52:24 +0900

    EducationAddForm radio버튼 수정

--- front/src/components/education/EducationAddForm.js
+++ front/src/components/education/EducationAddForm.js
@@ -24,10 +24,14 @@ const EducationAddForm=({ user_id, setEducations, setIsAdding })=>{
                <input type="text" placeholder="학교 이름" value={school} onChange={(e)=>setSchool(e.target.value)}/>
                <input type="text" placeholder="전공" value={major} onChange={(e)=>setMajor(e.target.value)}/>
                <div onChange={(e)=>setPosition(e.target.value)}>
-                   <input type="radio" name="position" value="재학중"/>
-                   <input type="radio" name="position" value="학사졸업"/>
-                   <input type="radio" name="position" value="석사졸업"/>
-                   <input type="radio" name="position" value="박사졸업"/>
+                   <input type="radio" value="재학중" id="attending"/>
+                   <label htmlFor="attending">재학중</label>
+                   <input type="radio" value="학사졸업" id="bachelor"/>
+                   <label htmlFor="bachelor">학사졸업</label>
+                   <input type="radio" value="석사졸업" id="master"/>
+                   <label htmlFor="master">석사졸업</label>
+                   <input type="radio" value="박사졸업" id="phD"/>
+                   <label htmlFor="phD">박사졸업</label>
                </div>
                <button type="submit">확인</button>
                <button onClick={()=>setIsAdding(false)}>취소</button> 
