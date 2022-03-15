f8fe4843e044f19d6ac54378f3f42aab21d1f1b6
bsy1141 <bsy1141@naver.com>
2022-03-15 16:05:25 +0900

    educationAddForm 주석 추가

--- front/src/components/education/EducationAddForm.js
+++ front/src/components/education/EducationAddForm.js
@@ -1,10 +1,12 @@
 import React,{useState} from 'react'
 import * as Api from '../../api'
 const EducationAddForm=({ user_id, setEducations, setIsAdding })=>{
+    //학력 정보를 post할 때 필요한 school, major, position을 state로 지정
     const [school, setSchool]=useState("")
     const [major, setMajor]=useState("")
     const [position,setPosition]=useState("")
-
+    
+    //확인 버튼을 눌렀을 때 post요청 보낼 함수
     const handleSubmit=(e)=>{
         e.preventDefault()
         Api.post('education/create',{
@@ -13,8 +15,8 @@ const EducationAddForm=({ user_id, setEducations, setIsAdding })=>{
             major,
             position
         })
-        .then(res=>setEducations(res.data))
-        .then(()=>setIsAdding(false))
+        .then(res=>setEducations(res.data)) //add한 정보 바탕으로 새롭게 educations state를 세팅
+        .then(()=>setIsAdding(false)) //add 완료했으므로 폼을 닫아주기 위해 isadding state를 false로 세팅
     }
    return(
        <div>
@@ -28,7 +30,7 @@ const EducationAddForm=({ user_id, setEducations, setIsAdding })=>{
                    <input type="radio" name="position" value="박사졸업"/>
                </div>
                <button type="submit">확인</button>
-               <button onClick={()=>setIsAdding(false)}>취소</button>
+               <button onClick={()=>setIsAdding(false)}>취소</button> 
            </form>
        </div>
    )
