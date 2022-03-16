d926c3dd13464079fc56d579e16e434ecc8d5d3e
[4기-1반] 이예원 <panggun503@gmail.com>
2022-03-16 17:20:23 +0900

    merge - ProjectCard에 날짜를 자르는 slicingDate 함수 추가

--- front/src/components/project/ProjectCard.js
+++ front/src/components/project/ProjectCard.js
@@ -1,13 +1,9 @@
 import { Card, Button, Row, Col } from "react-bootstrap";
 
 function ProjectCard({ project, isEditable, setIsEditing }) {
-  // const convertDate = (date) => {
-  //   const year = date.getFullYear();
-  //   const month = ('0' + (date.getMonth() + 1)).slice(-2);
-  //   const day = ('0' + date.getDate()).slice(-2);
-  //   const selectedDate = [year, month, day].join("-");
-  //   return selectedDate;
-  // }
+  const slicingDate = (date) => {
+    return date.slice(0, 10);
+  }
   return (
     <Card.Text>
       {/* project의 제목, 상세내용, 기간 */}
@@ -18,7 +14,7 @@ function ProjectCard({ project, isEditable, setIsEditing }) {
           <span className="text-muted">{project.description}</span>
           <br />
           {/* project의 기간을 object로 받고 출력형식에 맞게 변경 */}
-          <span className="text-muted">{(project.from_date)} ~ {(project.to_date)}</span>
+          <span className="text-muted">{slicingDate(project.from_date)} ~ {slicingDate(project.to_date)}</span>
         </Col>
         {/* project 편집 버튼 */}
         {isEditable && (
