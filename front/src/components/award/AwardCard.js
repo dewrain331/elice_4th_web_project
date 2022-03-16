038759d536aea7f5fef6b1700f5e25102b780329
Isacc <dewrain331@gmail.com>
2022-03-16 10:16:45 +0900

    awardcard isEditbale 편집/삭제 버튼 적용

--- front/src/components/award/AwardCard.js
+++ front/src/components/award/AwardCard.js
@@ -1,10 +1,10 @@
-import { Card, Button, Row, Col } from "react-bootstrap"
+import { Card, Button, Row, Col, Modal } from "react-bootstrap"
+import {useState} from 'react'
 
 const AwardCard = ({ _award, isEditable, setIsEditing }) => {
-    // 삭제 버튼 클릭 시 함수
-    const handleDelete = () => {
-
-    }
+    const [show, setShow] = useState(false)
+    const handleShow = () => setShow(true)
+    const handleClose = () => setShow(false)
 
     return (
         <>
@@ -18,22 +18,37 @@ const AwardCard = ({ _award, isEditable, setIsEditing }) => {
                     </Col>
                     <Col xs lg="1">
                         {/* 각 항목마다 편집 버튼을 생성 */}
-                        <Button
+                        {isEditable && <Button
                             variant="outline-info"
                             size="sm"
                             onClick={() => setIsEditing(prev => !prev)}
                             className="mr-3"
-                        >편집</Button>
+                        >편집</Button>}
                         {/* 각 항목마다 삭제 버튼을 생성 */}
-                        <Button
+                        {isEditable && <Button
                             variant="outline-secondary"
                             size="sm"
-                            onClick={handleDelete}
+                            onClick={handleShow}
                             className="mr-3"
-                        >삭제</Button>
+                        >삭제</Button>}
                     </Col>
                 </Row>
             </Card.Text>
+
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                <Modal.Title>Modal heading</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+                <Modal.Footer>
+                <Button variant="secondary" onClick={handleClose}>
+                    Close
+                </Button>
+                <Button variant="primary" onClick={handleClose}>
+                    Save Changes
+                </Button>
+                </Modal.Footer>
+            </Modal>
         </>
     )
 }
