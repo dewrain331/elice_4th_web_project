8837f6ce877dd6544a3f53796705c8960cfa2c8b
bsy1141 <bsy1141@naver.com>
2022-03-16 10:23:03 +0900

    Education컴포넌트를 Portfolio 컴포넌트에 추가

--- front/src/components/education/Educations.js
+++ front/src/components/education/Educations.js
@@ -5,7 +5,7 @@ import EducationAddForm from './EducationAddForm';
 //Educations는 Portfolio.js에 추가되어야 함
 //백엔드서버/educations/아이디'에 사용자 포트폴리오가 저장되어 있으므로 아이디 정보 필요 
 //포트폴리오가 사용자의 것이면 addform, editbutton이 보여야 함
-const Educations = ({ portfolioOwnerId, isEditable, setIsAdding }) => {
+const Educations = ({ portfolioOwnerId, isEditable }) => {
     const [educations, setEducations]=useState([])
     const [isAdding, setIsAdding]=useState(false)
     useEffect(()=>{
@@ -14,7 +14,6 @@ const Educations = ({ portfolioOwnerId, isEditable, setIsAdding }) => {
 
     return(
         <div>
-            {/* 정상적으로 불러와지는 게 확인되면 EducationCard로 옮겨갈 부분 */}
             <h3>학력</h3>
             {educations.map((edu)=>(
                 <Education 
