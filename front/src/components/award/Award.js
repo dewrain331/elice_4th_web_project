f1584a7f64ae77fb6a8b7e7c9ceb60d592e6f175
Isacc <dewrain331@gmail.com>
2022-03-16 11:05:24 +0900

    delete 기능 구현?

--- front/src/components/award/Award.js
+++ front/src/components/award/Award.js
@@ -6,6 +6,7 @@ const Award = ({ _award, setAwards, isEditable }) => {
     // useState를 이용하여 '수정중' 상태를 관리
     // 최초에는 수정중이 아니므로, 초기값은 false로 설정.
     const [isEditing, setIsEditing] = useState(false)
+    
     return (
         <>
             {/* isEditing에 따라 수정중(true)이라면 수정 양식(AwardEditForm)을 보내고, 아니라면 Award 목록(AwardCard)을 표시 */}
