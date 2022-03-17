144d199f5332aa502167dadfbbc66291c2e2db32
bsy1141 <bsy1141@naver.com>
2022-03-17 14:49:37 +0900

    feat: pagination 구현

--- front/src/components/education/Educations.js
+++ front/src/components/education/Educations.js
@@ -1,18 +1,30 @@
 import React,{useEffect,useState} from 'react'
-import { Card, Button, Row, Col } from 'react-bootstrap'
-import * as Api from '../../api'
-import EducationAddForm from './EducationAddForm';
+import { Card, Button, Row, Col} from 'react-bootstrap'
+//import * as Api from '../../api'
+import axios from "axios"
+import EducationAddForm from './EducationAddForm'
 import Education from './Education'
 
-//Educations는 Portfolio.js에 추가되어야 함
-//백엔드서버/educations/아이디'에 사용자 포트폴리오가 저장되어 있으므로 아이디 정보 필요 
-//포트폴리오가 사용자의 것이면 addform, editbutton이 보여야 함
 const Educations = ({ portfolioOwnerId, isEditable }) => {
+    const backendPortNumber = "5001";
+    const serverUrl = "http://" + window.location.hostname + ":" + backendPortNumber;
+
+    const [totalPage,setTotalPage]=useState(0)
+    const [page,setPage]=useState(1)
     const [educations, setEducations]=useState([])
     const [isAdding, setIsAdding]=useState(false)
+
     useEffect(()=>{
-        Api.get('educationlist', portfolioOwnerId).then((res)=>setEducations(res.data))
+        // Api.get('educationlist', portfolioOwnerId).then((res)=>setEducations(res.data))
+        axios
+        .get(`${serverUrl}/educationlist/${portfolioOwnerId}?page=${page}&perPage=3`)
+        .then((res)=>{
+            const {totalPage, educations}=res
+            setTotalPage(totalPage)
+            setEducations(educations)
+        })
     },[portfolioOwnerId])
+    
     return(
         <Card>
             <Card.Body>
@@ -37,6 +49,23 @@ const Educations = ({ portfolioOwnerId, isEditable }) => {
                     setEducations={setEducations}
                     setIsAdding={setIsAdding}
                 />}
+                <Row className="mt-3 text-center mb-4">
+                         <Col>
+                            <Button variant="outline-secondary" size="sm" 
+                                onClick={()=>setPage((prev)=>prev-1)}
+                                disabled={page===1}>
+                                {"<"}
+                            </Button>{" "}
+                            <Button variant="outline-secondary" size="sm" disabled>
+                                {page}/{totalPage}
+                            </Button>{" "}
+                            <Button variant="outline-secondary" size="sm" 
+                                onClick={()=>setPage((prev)=>prev+1)}
+                                disabled={page===totalPage}>
+                                {">"}
+                            </Button>
+                        </Col>
+                    </Row>
             </Card.Body>
         </Card>
     )
