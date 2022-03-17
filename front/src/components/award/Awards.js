b8cf0e148570d4df9e35b8c6a0f1c0d47c6ef5a4
Isacc <dewrain331@gmail.com>
2022-03-17 15:28:41 +0900

    feat : Award MVP Pagination
     Front Branch에 적용

--- front/src/components/award/Awards.js
+++ front/src/components/award/Awards.js
@@ -10,12 +10,25 @@ const Awards = ({ portfolioOwnerId, isEditable }) => {
     // useState로 생성 상태를 관리할 변수를 선언
     // 초기 상태는 생성 중이 아니므로, 초기값은 false
     const [isAdding, setIsAdding] = useState(false)
+    // Pagination 관련
+    const [page, setPage] = useState(1)
+    const [totalPage, setTotalPage] = useState(1)
 
     useEffect(() => {
         // DB에 저장된 유저의 Award들을 Awards 변수에 넣음.
-        Api.get("awardlist", portfolioOwnerId)
+        Api.get(`awardlist/${portfolioOwnerId}`)
             .then(res => setAwards(res.data))
     }, [portfolioOwnerId])
+        /*
+        // params 적용시 사용할 api.get
+        Api.get(`awardlist/${portfolioOwnerId}?page=${page}&perPage=3`)
+            .then(res => {
+                const {totalPage, awards} = res.data
+                setTotalPage(totalPage)
+                setAwards(awards)
+            })
+    }, [porfolioOwnerId, page]
+        */
 
     return (
         <Card>
@@ -43,6 +56,34 @@ const Awards = ({ portfolioOwnerId, isEditable }) => {
                         setIsAdding={setIsAdding}
                     />
                 )}
+                <Row className="mt-3 text-center mb-4">
+                    <Col>
+                        <Button
+                            variant="outline-secondary"
+                            size="sm"
+                            onClick={() => setPage(prev => prev - 1)}
+                            disabled={page === 1}
+                            className="me-3"
+                        >
+                            {"<"}
+                        </Button>
+                        <Button
+                            variant="outline-secondary"
+                            size="sm"
+                        >
+                            {page} / {totalPage}
+                        </Button>
+                        <Button
+                            variant="outline-secondary"
+                            size="sm"
+                            onClick={() => setPage(prev => prev + 1)}
+                            disabled={page === totalPage}
+                            className="ms-3"
+                        >
+                            {">"}
+                        </Button>
+                    </Col>
+                </Row>
             </Card.Body>
         </Card>
     )
