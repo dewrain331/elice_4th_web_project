1435415c9eff3278b3555169a02a48f48bb604f2
[4기-1반] 이예원 <panggun503@gmail.com>
2022-03-18 10:33:18 +0900

    chore: portfolio 기본 제공 텍스트 삭제

--- front/src/components/Portfolio.js
+++ front/src/components/Portfolio.js
@@ -60,18 +60,11 @@ function Portfolio() {
             isEditable={portfolioOwner.id === userState.user?.id}
           />
         </Col>
-        <Col md="3" lg="3">
+        <Col >
           <Certificates
             portfolioOwnerId={portfolioOwner.id}
             isEditable={portfolioOwner.id === userState.user?.id}
           />
-        </Col>
-        <Col>
-
-          <div style={{ textAlign: "center" }}>
-            학력 목록, 수상이력 목록, 프로젝트 목록, 자격증 목록 만들기
-          </div>
-
         </Col>
       </Row>
     </Container>
