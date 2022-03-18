4b1fba69145df4d6c0e5bde268159042c423cef8
12yoonj <12yoonj@gmail.com>
2022-03-18 13:08:42 +0900

    docs: image 구현 관련 주석

--- back/src/db/schemas/user.js
+++ back/src/db/schemas/user.js
@@ -1,5 +1,21 @@
 import { Schema, model } from "mongoose";
 
+const imageSchema = new mongoose.Schema(
+  {
+    imageName: {
+      type: String,
+      required: true,
+    },
+    path: {
+      type: String,
+      required: true,
+    }
+  },
+  {
+    timestamps: true,
+  }
+);
+
 const UserSchema = new Schema(
   {
     id: {
@@ -23,6 +39,13 @@ const UserSchema = new Schema(
       required: false,
       default: "설명이 아직 없습니다. 추가해 주세요.",
     },
+    image: {
+      type: imageSchema,
+      default: {
+        imageName: "default_kitten_image",
+        path: `http://placekitten.com/200/200`,
+      }
+    }
   },
   {
     timestamps: true,
@@ -32,3 +55,5 @@ const UserSchema = new Schema(
 const UserModel = model("User", UserSchema);
 
 export { UserModel };
+
+// `../images/${imageName}`
