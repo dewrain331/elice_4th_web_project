407d16520d062c83eadf89d4248b89ec4228b781
YeonSeok <dustjr1005@naver.com>
2022-03-18 10:29:45 +0900

    Feat : comment, Comments model  생성
    댓글 구현을 위한 기초작업
    
    해결

---
+++ back/src/db/schemas/comment.js
@@ -0,0 +1,34 @@
+import { Schema, model } from "mongoose";
+
+const CommentSchema = new Schema(
+  {
+    id: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: false,
+      default: "설명이 아직 없습니다. 추가해 주세요.",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const CommentModel = model("Comment", CommentSchema);
+
+export { CommentModel };
