5080d75727094827defa930ebe349a126e740c69
YeonSeok <dustjr1005@naver.com>
2022-03-18 10:13:16 +0900

    Fix : Awards 삭제
    사용하지 않는 Awards.js 삭제
    
    해결

--- back/src/db/index.js
+++ back/src/db/index.js
@@ -3,9 +3,6 @@ import { User } from "./models/User";
 import { Award } from "./models/Award";
 import { Certificate } from "./models/Certificate";
 
-// todo 원본 북구
-import { Awards } from "./models/Awards";
-
 const DB_URL =
   process.env.MONGODB_URL ||
   "MongoDB 서버 주소가 설정되지 않았습니다.\n./db/index.ts 파일을 확인해 주세요.";
@@ -20,5 +17,4 @@ db.on("error", (error) =>
   console.error("MongoDB 연결에 실패하였습니다...\n" + DB_URL + "\n" + error)
 );
 
-// todo 원본 북구
-export { User, Award, Certificate, Awards };
+export { User, Award, Certificate};
