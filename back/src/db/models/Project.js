758db6cc1b694416c08505e00fda1850a1c2c5b3
12yoonj <12yoonj@gmail.com>
2022-03-18 13:12:58 +0900

    fix: Project - total documents 수를 보내는 방식으로 변경

--- back/src/db/models/Project.js
+++ back/src/db/models/Project.js
@@ -13,9 +13,8 @@ class Project {
 
   static findByUserId = async ({ userId, page, perPage }) => {
     const total = await projectModel.countDocuments({ userId });
-    const totalPage = Math.ceil(total / perPage);
     const projects = await projectModel.find({ userId }).sort({ createdAt: 1 }).skip(perPage * (page -1)).limit(perPage);
-    return { totalPage, "projects": projects};
+    return { total, "projects": projects};
   }
 
   static update = async ({ projectId, userId, fieldToUpdate, newValue }) => {
