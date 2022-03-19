1484c4831119268aa356ad5d531a23656db3aaa4
YeonSeok <dustjr1005@naver.com>
2022-03-19 17:27:52 +0900

    zzzzz

---
+++ front/src/components/certificate/CertificateCard.js
@@ -0,0 +1,84 @@
+import { Card, Button, Row, Col, Modal } from "react-bootstrap"
+import {useState} from 'react'
+import * as Api from "../../api"
+
+const CertificateCard = ({ certificate, isEditable, setIsEditing, setCertificates, setAllPage, page, setPage }) => {
+    // Modal 관련 State
+    const slicingDate = (date) => {
+        return date.slice(0, 10)
+    }
+
+    const [show, setShow] = useState(false)
+    const handleShow = () => setShow(true)
+    const handleClose = () => setShow(false)
+
+    const handleDelete = async () => {
+        try {
+            const {id, user_id} = certificate
+            await Api.delete(`certificate/${id}`)
+            const res = await Api.get("certificatelist", `${user_id}?page=${page}&perPage=3`)
+            const {total, certificates} = res.data
+            if(page > Math.ceil(total / 3)) {
+                setPage(page - 1)
+            }
+            setAllPage(Math.ceil(total / 3))
+            setCertificates(certificates)
+            setShow(false)
+        } catch (err) {
+            console.error(err)
+        }
+    }
+
+    return (
+        <>
+            <Card.Body>
+                {/* certificate의 자격증 이름과 상세내용, 취득일자를 출력 */}
+                <Row className="align-items-center">
+                    <Col>
+                        <span>{certificate.title}</span>
+                        <br />
+                        <span className="text-muted">{certificate.description}</span>
+                        <br />
+                        <span className="text-muted">{slicingDate(certificate.date)}</span>
+                    </Col>
+                    <Col xs lg="1">
+                        {/* 각 항목마다 편집 버튼을 생성 */}
+                        {isEditable && <Button
+                            variant="outline-info"
+                            size="sm"
+                            onClick={() => setIsEditing(prev => !prev)}
+                            className="mr-3"
+                        >편집</Button>}
+                        {/* 각 항목마다 삭제 버튼을 생성 */}
+                        {isEditable && <Button
+                            variant="outline-secondary"
+                            size="sm"
+                            onClick={handleShow}
+                        >삭제</Button>}
+                    </Col>
+                </Row>
+            </Card.Body>
+
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                <Modal.Title>삭제 확인</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>정말로 삭제하시겠습니까?</Modal.Body>
+                <Modal.Footer>
+                <Button variant="secondary" onClick={handleClose}>
+                    취소
+                </Button>
+                <Button variant="danger" onClick={() => {
+                    handleClose()
+                    handleDelete()
+                    }
+                }>
+                    삭제
+                </Button>
+                </Modal.Footer>
+            </Modal>
+        </>
+    )
+}
+
+export default CertificateCard
---
+++ front/src/components/certificate/CertificateEditForm.js
@@ -0,0 +1,87 @@
+import { useState } from "react"
+import { Button, Form, Col, Row } from "react-bootstrap"
+import * as Api from "../../api"
+import DatePicker from 'react-datepicker'
+
+const CertificateEditForm = ({ currentCertificate, setCertificates, setIsEditing, page }) => {
+    // 편집 버튼을 누른 항목의 자격증 제목을 담을 title 변수 선언.
+    const [title, setTitle] = useState(currentCertificate.title)
+    // 편집 버튼을 누른 항목의 상세내용을 담을 description 변수 선언.
+    const [description, setDescription] = useState(currentCertificate.description)
+    // 편집 버튼을 누른 항목의 취득일자를 담을 date 변수 선언.
+    const [date, setDate] = useState(new Date(currentCertificate.date))
+
+    const handleSubmit = async (evt) => {
+        // Form의 기본 기능 막기.
+        evt.preventDefault()
+        evt.stopPropagation()
+
+        const user_id = currentCertificate.user_id
+
+        // put 요청.
+        try {
+            await Api.post(`certificate/${currentCertificate.id}`, {
+                title,
+                description,
+                date: date.toJSON()
+            })
+        } catch (err) {
+            console.error(err)
+        }
+
+        // put 요청값과 함께 각각의 Certificate들의 모임인 Certificates를 다시 렌더링
+        try {
+            const res = await Api.get("certificatelist", `${user_id}?page=${page}&perPage=3`)
+            setCertificates(res.data.certificates)
+            // 편집 상태 종료.
+            setIsEditing(false)
+        } catch (err) {
+            console.error(err)
+        }
+    }
+
+    return (
+        <>
+            <Form onSubmit={handleSubmit}>
+                <Form.Group controlId="formBasicTitle">
+                    <Form.Control
+                        type="text"
+                        placeholder="자격증 제목"
+                        value={title}
+                        onChange={evt => setTitle(evt.target.value)}
+                    />
+                </Form.Group>
+
+                <Form.Group controlId="formBasicDescription" className="mt-3">
+                    <Form.Control
+                        type="text"
+                        placeholder="상세내역"
+                        value={description}
+                        onChange={evt => setDescription(evt.target.value)}
+                    />
+                </Form.Group>
+
+                <Form.Group controlId="formBasicDate" className="mt-3">
+                    <DatePicker dateFormat="yyyy-MM-dd" selected={new Date(date)} onChange={v => setDate(v)} />
+                </Form.Group>
+
+                <Form.Group as={Row} className="mt-3 text-center">
+                    <Col sm={{ span: 20 }}>
+                        <Button
+                            variant="primary"
+                            type="submit"
+                            className="me-3"
+                        >확인</Button>
+                        <Button
+                            variant="secondary"
+                            type="button"
+                            onClick={() => setIsEditing(false)}
+                        >취소</Button>
+                    </Col>
+                </Form.Group>
+            </Form>
+        </>
+    )
+}
+
+export default CertificateEditForm
