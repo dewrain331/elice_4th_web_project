cb039617948b33015dfe4a968111e43525122b22
[4기-1반] 이예원 <panggun503@gmail.com>
2022-03-22 14:22:13 +0900

    fix: 필요없는 props 제거

--- front/src/components/project/Projects.js
+++ front/src/components/project/Projects.js
@@ -49,12 +49,7 @@ function Projects({ portfolioOwnerId, isEditable }) {
       <Card.Body>
         <Card.Title>프로젝트</Card.Title>
         {projects.map((project) => (
-          <Project
-            key={project.id}
-            project={project}
-            setProjects={setProjects}
-            isEditable={isEditable}
-          />
+          <Project key={project.id} project={project} isEditable={isEditable} />
         ))}
         {isEditable && (
           <Row className="mt-3 text-center mb-4">
