20700333fdf4ce8b589b7de9edda65b2d94af68e
12yoonj <12yoonj@gmail.com>
2022-03-22 16:33:21 +0900

    feat: like - 좋아요 기능 추가

--- back/src/db/models/User.js
+++ back/src/db/models/User.js
@@ -33,6 +33,59 @@ class User {
     );
     return updatedUser;
   }
+
+  static addLike = async ({ user_id, currentUserId }) => {
+    const owner = await UserModel.findOne({ id: user_id });
+    const likeUserList = owner.likeUsers;
+    // likeUserList.map((likeUser)=>{
+    //   if (likeUser === currentUserId) throw new Error('이미 좋아요를 눌렀습니다.');
+    // })
+    const userCheck = likeUserList.includes(currentUserId);
+    if (userCheck) {
+      throw new Error('이미 좋아요를 눌렀습니다.');
+    }
+    const addLikeCount = owner.likeCount + 1;
+
+    const filter = { id: user_id };
+    const update = { likeCount: addLikeCount, $push: { likeUsers: currentUserId } }
+    const option = { returnOriginal: false };
+
+    const addLikeUser = await UserModel.findOneAndUpdate(
+      filter,
+      update,
+      option
+    );
+    return addLikeUser;
+  }
+
+  static removeLike = async ({ user_id, currentUserId }) => {
+    const owner = await UserModel.findOne({ id: user_id });
+    const likeUserList = owner.likeUsers;
+    // likeUserList.map((likeUser)=>{
+    //   if (likeUser === currentUserId) {
+    //     return owner.likeCount;
+    //   }
+    //   else {
+    //     throw new Error('좋아요를 누른 적이 없습니다.')
+    //   }
+    // })
+    const userCheck = likeUserList.includes(currentUserId);
+    if (!userCheck) {
+      throw new Error('좋아요를 누른 적이 없습니다.');
+    }
+    const subtractLikeCount = owner.likeCount - 1;
+
+    const filter = { id: user_id };
+    const update = { likeCount: subtractLikeCount, $pull: { likeUsers: currentUserId } }
+    const option = { returnOriginal: false };
+
+    const removeLikeUser = await UserModel.findOneAndUpdate(
+      filter,
+      update,
+      option
+    );
+    return removeLikeUser;
+  }
 }
 
 export { User };
