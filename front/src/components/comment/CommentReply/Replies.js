282fa07ce73d46a07809a38fb2f4e2494683d4f2
bsy1141 <bsy1141@naver.com>
2022-03-24 10:56:08 +0900

    feat : reply edit 기능 구현

--- front/src/components/comment/CommentReply/Replies.js
+++ front/src/components/comment/CommentReply/Replies.js
@@ -1,37 +1,29 @@
-import { useState, useEffect } from 'react'
-import * as Api from '../../../api'
-import Reply from './Reply'
-import ReplyAddForm from './ReplyAddForm'
+import { useState, useEffect } from "react";
+import * as Api from "../../../api";
+import Reply from "./Reply";
+import ReplyAddForm from "./ReplyAddForm";
 
-const Replies=({ id, author })=>{
-    const [replies, setReplies]=useState([])
+const Replies = ({ id, author }) => {
+  const [replies, setReplies] = useState([]);
 
-    useEffect(()=>{
-        Api
-        .get('comment', id)
-        .then(res=>setReplies(res.data[0].replys))
-    },[id])
+  useEffect(() => {
+    Api.get("comment", id).then((res) => setReplies(res.data[0].replys));
+  }, [id]);
 
-    console.log(replies)
-    return(
-        <>
-            {
-                replies && replies.map(reply=>(
-                    <Reply
-                        key={reply.id} 
-                        reply={reply}
-                        isEditable={author.id===reply.author_id}
-                        setReplies={setReplies}
-                        />
-                ))
-            }
-            <ReplyAddForm 
-                author={author} 
-                CommentId={id}
-                setReplies={setReplies}
-            />
-        </>
-    )
-}
+  return (
+    <>
+      {replies &&
+        replies.map((reply) => (
+          <Reply
+            key={reply.id}
+            reply={reply}
+            isEditable={author.id === reply.author_id}
+            setReplies={setReplies}
+          />
+        ))}
+      <ReplyAddForm author={author} CommentId={id} setReplies={setReplies} />
+    </>
+  );
+};
 
-export default Replies
+export default Replies;
--- front/src/components/comment/CommentReply/Reply.js
+++ front/src/components/comment/CommentReply/Reply.js
@@ -1,43 +1,28 @@
-import { Card, Col, Row, Button } from 'react-bootstrap'
-import * as Api from '../../../api'
+import { useState } from "react";
+import ReplyEditForm from "./ReplyEditForm";
+import ReplyCard from "./ReplyCard";
 
-const Reply=({ reply, isEditable, setReplies })=>{
-    const { id, author_name, text, parent_comment_id, createdAt } = reply
+const Reply = ({ reply, isEditable, setReplies }) => {
+  const [isEditing, setIsEditing] = useState(false);
 
-    const handleDelete= async ()=>{
-        try{
-            await Api.delete('comment/reply',`${parent_comment_id}/${id}`)
-            
-            setReplies((replies)=>{
-                const pos=replies.findIndex(r=>r.id===id)
-                if(pos>=-1){
-                    replies.splice(pos,1)
-                    return [...replies]
-                }
-                return replies
-            })
-            
-        }catch(err){
-            console.log(err.message)
-        }
-    }
+  return (
+    <>
+      {isEditing ? (
+        <ReplyEditForm
+          reply={reply}
+          setReplies={setReplies}
+          setIsEditing={setIsEditing}
+        />
+      ) : (
+        <ReplyCard
+          reply={reply}
+          setReplies={setReplies}
+          isEditable={isEditable}
+          setIsEditing={setIsEditing}
+        />
+      )}
+    </>
+  );
+};
 
-    return(
-        <Card.Body className="mt-2 text-justify float-left">
-            <Card className="p-3">
-                <Row>
-                    <Col xs={10} className="text-primary ">
-                        <h4>{author_name}</h4></Col>
-                    <Col xs={2} className="text-secondary">
-                        <h6>{createdAt.slice(0,10)}</h6> </Col>
-                </Row>
-                <Card.Text className="mt-2">{text}</Card.Text>
-                <Col>
-                    { isEditable && <Button variant="secondary" onClick={ handleDelete }>Delete</Button>}
-                </Col>
-            </Card>
-        </Card.Body>
-    )
-}
-
-export default Reply
+export default Reply;
