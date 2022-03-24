941009d0d24fe026522c76f4991fc3eec02c930d
bsy1141 <bsy1141@naver.com>
2022-03-24 12:36:23 +0900

    fix: comment add form destructiong 오류 수정

--- front/src/components/comment/CommentAddForm.js
+++ front/src/components/comment/CommentAddForm.js
@@ -1,33 +1,37 @@
-import { useState } from 'react'
-import { Form, Button } from 'react-bootstrap'
-import * as Api from '../../api'
+import { useState } from "react";
+import { Form, Button } from "react-bootstrap";
+import * as Api from "../../api";
 
-const CommentAddForm=({ user_id, author, setComments })=>{
-    const { id, name } = author
-    const [comment,setComment]=useState("")
-    const handleSubmit= async (e)=>{
-        e.preventDefault()
-        //post
-        const res=await Api.post('comment', {
-            user_id,
-            author_id : id,
-            author_name : name,
-            text : comment
-        })
-        
-        setComments((comments)=>[...comments,res.data])
-        setComment("")
-    }
+const CommentAddForm = ({ user_id, author, setComments }) => {
+  const [comment, setComment] = useState("");
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-    return(
-        <Form onSubmit={handleSubmit}>
-            <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-                <Form.Label>댓글을 입력해주세요!</Form.Label>
-                <Form.Control as="textarea" rows={3} value={comment} onChange={(e)=>setComment(e.target.value)}/>
-            </Form.Group>
-            <Button type="submit">Submit</Button>
-        </Form>
-    )
-}
+    const res = await Api.post("comment", {
+      user_id,
+      author_id: author?.id,
+      author_name: author?.name,
+      text: comment,
+    });
 
-export default CommentAddForm
+    setComments((comments) => [...comments, res.data]);
+    setComment("");
+  };
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+        <Form.Label>댓글을 입력해주세요!</Form.Label>
+        <Form.Control
+          as="textarea"
+          rows={3}
+          value={comment}
+          onChange={(e) => setComment(e.target.value)}
+        />
+      </Form.Group>
+      <Button type="submit">Submit</Button>
+    </Form>
+  );
+};
+
+export default CommentAddForm;
