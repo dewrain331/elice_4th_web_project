d799349070a00b497c99c45436c036c008cae79f
[4기-1반] 이예원 <panggun503@gmail.com>
2022-03-26 23:16:04 +0900

    chore: galleryRouter test를 위한 수정

--- back/src/routers/galleryRouter.js
+++ back/src/routers/galleryRouter.js
@@ -5,61 +5,65 @@ import { galleryService } from "../services/galleryService";
 import multer from "multer";
 
 const galleryRouter = Router();
-galleryRouter.use(login_required)
+galleryRouter.use(login_required);
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, '../front/public/images/')
+    cb(null, "../front/public/build/images/");
   },
   filename: function (req, file, cb) {
-    cb(null, `${file.fieldname}_`+ Date.now() + `_${file.originalname}`)
-  }
-})
+    cb(null, `${file.fieldname}_` + Date.now() + `_${file.originalname}`);
+  },
+});
 
-const upload = multer({ storage: storage }) // storage에 저장, 이미지 크기는 5MB로 제한
+const upload = multer({ storage: storage }); // storage에 저장, 이미지 크기는 5MB로 제한
 
-galleryRouter.post("/gallery/create", upload.single('gallery'), async function (req, res, next) {
-  try {
-    if (is.emptyObject(req.body)) {
-      throw new Error(
-        "요청 내용이 빈 객체입니다. headers의 Content-Type을 application/json으로 설정해주세요"
-      );
-    }
+galleryRouter.post(
+  "/gallery/create",
+  upload.single("gallery"),
+  async function (req, res, next) {
+    try {
+      if (is.emptyObject(req.body)) {
+        throw new Error(
+          "요청 내용이 빈 객체입니다. headers의 Content-Type을 application/json으로 설정해주세요"
+        );
+      }
 
-    // req (request) 에서 데이터 가져오기
-    const { userId, description } = req.body;
+      // req (request) 에서 데이터 가져오기
+      const { userId, description } = req.body;
 
-    // req.file 은 `gallery` 라는 필드의 파일 정보입니다.
-    const saveFileName = req.file.filename; // 저장된 파일명​ 
-    const saveFilePath = `/images/${saveFileName}` // 업로드된 파일의 경로 (index.html 기준)
-    const newImage = { userId, description, saveFileName, saveFilePath };
+      // req.file 은 `gallery` 라는 필드의 파일 정보입니다.
+      const saveFileName = req.file.filename; // 저장된 파일명​
+      const saveFilePath = `/images/${saveFileName}`; // 업로드된 파일의 경로 (index.html 기준)
+      const newImage = { userId, description, saveFileName, saveFilePath };
 
-    const newImageContent = await galleryService.addImageContent( newImage );
+      const newImageContent = await galleryService.addImageContent(newImage);
 
-    if (newImageContent.errorMessage) {
+      if (newImageContent.errorMessage) {
         throw new Error(newImageContent.errorMessage);
+      }
+
+      res.status(201).json(newImageContent);
+    } catch (error) {
+      next(error);
     }
-    
-    res.status(201).json(newImageContent);
+  }
+);
+
+galleryRouter.delete("/gallery/:userId/:id", async function (req, res, next) {
+  try {
+    const imageId = req.params.id;
+    const result = await galleryService.deleteImage({ imageId });
+
+    if (result.errorMessage) {
+      throw new Error(result.errorMessage);
+    }
+
+    res.status(200).send(result);
   } catch (error) {
     next(error);
   }
-}); 
-
-galleryRouter.delete("/gallery/:userId/:id", async function (req, res, next) {
-      try {
-        const imageId = req.params.id;
-        const result = await galleryService.deleteImage({ imageId });
-
-        if (result.errorMessage) {
-          throw new Error(result.errorMessage);
-        }
-    
-        res.status(200).send(result);
-      } catch (error) {
-        next(error);
-      }
-}); 
+});
 
 galleryRouter.get("/gallery/:userId/:id", async function (req, res, next) {
   try {
@@ -84,22 +88,28 @@ galleryRouter.get("/gallery/:userId", async function (req, res, next) {
     // 특정 사용자의 전체 프로젝트 목록을 얻음
     const userId = req.params.userId;
 
-    const { images } = await galleryService.getGallery({ userId })
-    
+    const { images } = await galleryService.getGallery({ userId });
+
     res.status(200).send({ images });
   } catch (error) {
     next(error);
   }
 });
 
-galleryRouter.patch("/gallery/:userId/:id", upload.none(), async function (req, res, next) {
+galleryRouter.patch(
+  "/gallery/:userId/:id",
+  upload.none(),
+  async function (req, res, next) {
     try {
       // URI로부터 data id를 추출함.
       const imageId = req.params.id;
       const { description } = req.body ?? null;
 
       // 위 추출된 정보를 이용하여 db의 데이터 수정함
-      const updatedImageContent = await galleryService.setImageContent({ imageId, description });
+      const updatedImageContent = await galleryService.setImageContent({
+        imageId,
+        description,
+      });
 
       if (updatedImageContent.errorMessage) {
         throw new Error(updatedImageContent.errorMessage);
