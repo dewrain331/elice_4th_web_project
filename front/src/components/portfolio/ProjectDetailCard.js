e1d6c7f26f13e5f6430296216471786c75ed782b
bsy1141 <bsy1141@naver.com>
2022-04-11 22:28:57 +0900

    chore: card-date부분 수정

--- front/src/components/portfolio/ProjectDetailCard.js
+++ front/src/components/portfolio/ProjectDetailCard.js
@@ -1,11 +1,6 @@
 import { Card, Col, Button } from "react-bootstrap";
 
 const ProjectDetailCard = ({ project, setIsEditing, isEditable }) => {
-  const slicingDate = (from, to) => {
-    from = from.slice(0, 10).split("-").join(".");
-    to = to.slice(0, 10).split("-").join(".");
-    return `${from} ~ ${to}`;
-  };
   return (
     <Card.Body
       className="mb-4"
@@ -20,9 +15,7 @@ const ProjectDetailCard = ({ project, setIsEditing, isEditable }) => {
       >
         {project.title}
       </h4>
-      <p style={{ backgroundColor: "white" }}>
-        {slicingDate(project.fromDate, project.toDate)}
-      </p>
+      <p style={{ backgroundColor: "white" }}>{project.date}</p>
       {project.deployLink && (
         <div style={{ backgroundColor: "white" }}>
           <span style={{ backgroundColor: "white", fontWeight: "bold" }}>
