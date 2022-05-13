4232d34ca63280fe87b7f4a52a43ed10eedc17b9
12yoonj <12yoonj@gmail.com>
2022-05-13 13:59:22 +0900

    chore: dm 임시 커밋

--- back/index.js
+++ back/index.js
@@ -1,8 +1,12 @@
 import 'dotenv/config'
 import { app } from "./src/app";
-
+import SocketIO from "socket.io"
+ 
 const PORT = process.env.SERVER_PORT || 5000;
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`정상적으로 서버를 시작하였습니다.  http://localhost:${PORT}`);
 });
+
+// 서버 연결, path는 프론트와 일치시켜준다.
+export const io = SocketIO(server, { path: '/socket.io' });
