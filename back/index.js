b86c68a2e16f14610132a59850f5aec0bf77fcdd
12yoonj <12yoonj@gmail.com>
2022-05-17 21:17:59 +0900

    feat: socket 적용

--- back/index.js
+++ back/index.js
@@ -1,6 +1,7 @@
 import 'dotenv/config'
 import { app } from "./src/app";
-import SocketIO from "socket.io"
+import { Server } from "socket.io"
+import { connectSocket } from "./socket.js"
  
 const PORT = process.env.SERVER_PORT || 5000;
 
@@ -8,5 +9,7 @@ const server = app.listen(PORT, () => {
   console.log(`정상적으로 서버를 시작하였습니다.  http://localhost:${PORT}`);
 });
 
-// 서버 연결, path는 프론트와 일치시켜준다.
-export const io = SocketIO(server, { path: '/socket.io' });
+const io = new Server(server);
+connectSocket()
+
+export { io }
--- back/socket.js
+++ back/socket.js
@@ -1,33 +1,33 @@
 import { io } from "./index.js"
 
 //* 웹소켓 연결 시
-io.on('connection', (socket) => {
-const req = socket.request; // 웹소켓과는 달리 req객체를 따로 뽑아야함
+async function connectSocket() {
+    io.on('connection', (socket) => {
+        console.log('a user connected: ', socket.id);
 
-//* ip 정보 얻기
-const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-console.log('새로운 클라이언트 접속!', ip, socket.id, req.ip);
-// socket.id 는 소켓 연결된 고유한 클라이언트 식별자라고 보면된다. 채팅방의 입장한 고유한 사람
+        socket.on('disconnect', () => {
+          console.log('a user disconnected: ', socket.id);
+        });
+      
+        socket.on('leaveRoom', (roomId) => {
+          console.log("leave", roomId)
+          socket.leave(roomId);
+        });
+      
+        socket.on('joinRoom', (roomId) => {
+          console.log("join", roomId)
+          socket.join(roomId);
+          // io.to(roomId).emit('joinRoom', roomId);
+        });
+      
+        socket.on('ping', (msg, roomId) => {
+          console.log(msg, roomId);   
+          socket.broadcast.to(roomId).emit('pong', msg);
+          // socket.emit('pong', msg)
+        });
 
-//* 연결 종료 시
-socket.on('disconnect', () => {
-console.log('클라이언트 접속 해제', ip, socket.id);
-clearInterval(socket.interval);
-});
+    })
+}
 
-//* 에러 시
-socket.on('error', (error) => {
-console.error(error);
-});
 
-//* 클라이언트로부터 메시지
-socket.on('reply', (data) => {
-console.log(data);
-});
-
-//* 클라이언트로 메세지 보내기
-socket.interval = setInterval(() => {
-    // 3초마다 클라이언트로 메시지 전송
-    socket.emit('news', 'Hello Socket.IO');
-    }, 3000)
-})
+export { connectSocket }
