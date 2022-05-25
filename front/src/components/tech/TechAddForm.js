72407f6d80e328ffd3987577a93da7f2dc1a5ab1
Isacc <dewrain331@gmail.com>
2022-05-25 14:35:16 +0900

    feat : Tech MVP 프론트엔드 부분 변경
    아이콘 삭제

--- front/src/components/tech/TechAddForm.js
+++ front/src/components/tech/TechAddForm.js
@@ -2,135 +2,123 @@ import { useState } from "react";
 import { Button, Form, Col, Row } from "react-bootstrap";
 import * as Api from "../../api";
 import { useSetRecoilState } from "recoil";
-import {
-  isAddingState,
-  techsState,
-} from "./TechAtom";
+import { isAddingState, techsState } from "./TechAtom";
 import "../Components.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { 
-  faJsSquare as javascript, 
-  faHtml5 as html, 
-  faCss3Alt as css, 
-  faReact as react, 
-  faNodeJs as nodejs, 
-  faJava as java 
-} from "@fortawesome/free-brands-svg-icons"
 
 const TechAddForm = ({ portfolioOwnerId }) => {
-  // RecoilStates
-  const setIsAdding = useSetRecoilState(isAddingState)
-  const setTechs = useSetRecoilState(techsState);
+	// RecoilStates
+	const setIsAdding = useSetRecoilState(isAddingState);
+	const setTechs = useSetRecoilState(techsState);
 
-  // useState로 기술 이름을 담을 title 변수 선언.
-  const [title, setTitle] = useState("");
-  // useState로 상세내용을 담을 description 변수 선언.
-  const [description, setDescription] = useState("");
-  // useState로 주관적 퍼센테이지를 담을 percent 변수 선언
-  const [percent, setPercent] = useState()
-  // 아이콘 추가 시 skills 리스트와 상위 import에 추가할 것.
-  const skills = [javascript, html, css, react, nodejs, java]
+	// useState로 기술 이름을 담을 title 변수 선언.
+	const [title, setTitle] = useState("");
+	// useState로 상세내용을 담을 description 변수 선언.
+	const [description, setDescription] = useState("");
+	// useState로 주관적 퍼센테이지를 담을 percent 변수 선언
+	const [percent, setPercent] = useState();
+	// 아이콘 추가 시 skills 리스트와 상위 import에 추가할 것.
+	const skills = [
+		"javascript",
+		"html",
+		"css",
+		"react",
+		"nodejs",
+		"java",
+		"bootstrap",
+		"mongodb",
+		"mysql",
+		"aws",
+		"docker",
+		"express",
+	];
 
-  const makeIcons = (skillName) => {
-    return (
-      <>
-        <FontAwesomeIcon
-          icon={skillName}
-          onClick={() => {
-            if(skillName.iconName === 'js-square') {
-              setTitle('javascript')
-            } else if(skillName.iconName === 'html5') {
-              setTitle(`html`)
-            } else if(skillName.iconName === 'css3-alt') {
-              setTitle(`css`)
-            } else if(skillName.iconName === 'react') {
-              setTitle(`react`)
-            } else if(skillName.iconName === 'node-js') {
-              setTitle(`nodejs`)
-            } else if(skillName.iconName === 'java') {
-              setTitle(`java`)
-            }
-          }}
-          style={{fontSize: '40px', cursor: 'pointer', marginLeft: '15px', marginRight: '15px', background: 'white'}}
-        />
-      </>
-    )
-  }
+	const makeSkillsInput = (skillName, index) => {
+		return (
+			<Form.Check
+				className="portfolioBG"
+				inline
+				label={skillName}
+				value={skillName}
+				name="skillName"
+				type="radio"
+				key={`inline-radio-${index}`}
+				onChange={(evt) => setTitle(evt.target.value)}
+			/>
+		);
+	};
 
-  const handleSubmit = async (evt) => {
-    // Form의 기본기능을 막기 위한 코드 선언.
-    evt.preventDefault();
-    evt.stopPropagation();
+	const handleSubmit = async (evt) => {
+		// Form의 기본기능을 막기 위한 코드 선언.
+		evt.preventDefault();
+		evt.stopPropagation();
 
-    const userId = portfolioOwnerId;
+		const userId = portfolioOwnerId;
 
-    // post 요청
-    try {
-      await Api.post("tech/create", {
-        userId,
-        title,
-        description,
-        percent
-      });
-    } catch (err) {
-      console.error(err);
-    }
+		// post 요청
+		try {
+			await Api.post("tech/create", {
+				userId,
+				title,
+				description,
+				percent,
+			});
+		} catch (err) {
+			console.error(err);
+		}
 
-    // post 요청값과 함께 각각의 tech들의 모임인 techs를 다시 렌더링
-    try {
-      const res = await Api.get(
-        "techList", `${userId}`
-      );
-      setTechs(res.data);
-      // 생성 상태 종료.
-      setIsAdding(false);
-    } catch (err) {
-      console.error(err);
-    }
-  };
+		// post 요청값과 함께 각각의 tech들의 모임인 techs를 다시 렌더링
+		try {
+			const res = await Api.get("techList", `${userId}`);
+			setTechs(res.data);
+			// 생성 상태 종료.
+			setIsAdding(false);
+		} catch (err) {
+			console.error(err);
+		}
+	};
 
-  return (
-    <Form onSubmit={handleSubmit} className="portfolioBG">
-      <Form.Group controlId="formBasicTitle" style={{background: 'white'}}>
-        {skills.map(v => makeIcons(v))}
-      </Form.Group>
+	return (
+		<Form onSubmit={handleSubmit} className="portfolioBG">
+			<Form.Group controlId="formBasicTitle" className="portfolioBG">
+				{skills.map((v, idx) => makeSkillsInput(v, idx))}
+			</Form.Group>
 
-      <Form.Group controlId="formBasicDescription" className="mt-3">
-        <Form.Control
-          type="text"
-          placeholder="간단한 설명을 적어주세요."
-          value={description}
-          onChange={(evt) => setDescription(evt.target.value)}
-        />
-      </Form.Group>
+			<Form.Group controlId="formBasicDescription" className="mt-3">
+				<Form.Control
+					type="text"
+					placeholder="간단한 설명을 적어주세요."
+					value={description}
+					onChange={(evt) => setDescription(evt.target.value)}
+				/>
+			</Form.Group>
 
-      <Form.Group controlId="formBasicPercent" className="mt-3">
-        <Form.Control
-          type="number"
-          placeholder="주관적 퍼센테이지"
-          value={percent}
-          min="1"
-          max="100"
-          onChange={(evt) => setPercent(evt.target.value)}
-        />
-      </Form.Group>
+			<Form.Group controlId="formBasicPercent" className="mt-3">
+				<Form.Control
+					type="number"
+					placeholder="주관적 퍼센테이지"
+					value={percent}
+					min="1"
+					max="100"
+					onChange={(evt) => setPercent(evt.target.value)}
+				/>
+			</Form.Group>
 
-      <Form.Group as={Row} className="mt-3 text-center">
-        <Col sm={{ span: 20 }} className="portfolioBG">
-          <Button variant="primary" type="submit" className="me-3">
-            확인
-          </Button>
-          <Button
-            variant="secondary"
-            type="button"
-            onClick={() => setIsAdding(false)}
-          >
-            취소
-          </Button>
-        </Col>
-      </Form.Group>
-    </Form>
-  );
+			<Form.Group as={Row} className="mt-3 text-center">
+				<Col sm={{ span: 20 }} className="portfolioBG">
+					<Button variant="primary" type="submit" className="me-3">
+						확인
+					</Button>
+					<Button
+						variant="secondary"
+						type="button"
+						onClick={() => setIsAdding(false)}
+					>
+						취소
+					</Button>
+				</Col>
+			</Form.Group>
+		</Form>
+	);
 };
 
-export default TechAddForm
+export default TechAddForm;
--- front/src/components/tech/TechCard.js
+++ front/src/components/tech/TechCard.js
@@ -6,122 +6,128 @@ import { useSetRecoilState } from "recoil";
 import { techsState } from "./TechAtom";
 
 const TechCard = ({ tech, isEditable, setIsEditing }) => {
-  // Modal 관련 State
-  const [show, setShow] = useState(false);
-  const handleAlertShow = () => setShow(true);
-  const handleAlertCancel = () => setShow(false);
+	// Modal 관련 State
+	const [show, setShow] = useState(false);
+	const handleAlertShow = () => setShow(true);
+	const handleAlertCancel = () => setShow(false);
 
-  // recoil 적용
-  const setTechs = useSetRecoilState(techsState);
+	// recoil 적용
+	const setTechs = useSetRecoilState(techsState);
 
-  // 삭제 기능을 하는 함수
-  const handleDelete = async () => {
-    const { id, userId } = tech;
-    try {
-      await Api.delete("techs", id);
-      setShow(false);
-      // "tech/유저id" get 요청 후 setting
-      const res = await Api.get("techList", userId);
-      setTechs(res.data);
-    } catch (error) {
-      console.error(error);
-    }
-  };
+	// 삭제 기능을 하는 함수
+	const handleDelete = async () => {
+		const { id, userId } = tech;
+		try {
+			await Api.delete("techs", id);
+			setShow(false);
+			// "tech/유저id" get 요청 후 setting
+			const res = await Api.get("techList", userId);
+			setTechs(res.data);
+		} catch (error) {
+			console.error(error);
+		}
+	};
 
-  const color = (title) => {
-    if(title === 'javascript') {return "rgb(200, 170, 90)"}
-    if(title === 'html') {return "rgb(200, 130, 50)"}
-    if(title === 'css') {return "rgb(50, 100, 200)"}
-    if(title === 'react') {return "rgb(30, 170, 200)"}
-    if(title === 'nodejs') {return "rgb(70, 180, 100)"}
-    if(title === 'java') {return "rgb(200, 50, 50)"}
-  }
+	return (
+		<>
+			<Card.Body
+				className="align-items-center"
+				style={{
+					maxWidth: "400px",
+					backgroundColor: "white",
+					margin: "30px",
+				}}
+			>
+				<ListGroup variant="flush">
+					<ListGroup.Item>
+						<Card.Title style={{ background: "white" }}>
+							{tech.title.toUpperCase()}
+						</Card.Title>
+						<Card.Text style={{ background: "white" }}>
+							{tech.description}
+						</Card.Text>
+					</ListGroup.Item>
+					<ListGroup.Item>
+						<div
+							style={{
+								width: "340px",
+								borderRadius: "40px",
+								background: "#ccc",
+								height: "40px",
+							}}
+						>
+							<span
+								style={{
+									display: "block",
+									height: "40px",
+									textAlign: "right",
+									background: "rgb(200, 170, 90)",
+									lineHeight: "40px",
+									borderRadius: "40px",
+									width: `${tech.percent}%`,
+								}}
+							>
+								{tech.percent}%
+							</span>
+						</div>
+					</ListGroup.Item>
+				</ListGroup>
+				<br />
+				{/* tech 편집 버튼 */}
+				<div
+					style={{
+						display: "flex",
+						paddingLeft: "85px",
+						backgroundColor: "white",
+						marginLeft: "50px",
+					}}
+				>
+					{isEditable && (
+						<Button
+							variant="outline-info"
+							size="sm"
+							onClick={() => setIsEditing((prev) => !prev)}
+						>
+							편집
+						</Button>
+					)}
+					{/* tech 삭제 버튼 */}
+					{isEditable && (
+						<Button
+							variant="outline-secondary"
+							size="sm"
+							onClick={handleAlertShow}
+						>
+							삭제
+						</Button>
+					)}
+				</div>
+			</Card.Body>
 
-  return (
-    <>
-      <Card.Body className="align-items-center" style={{ maxWidth: "400px", backgroundColor: "white", margin: "30px" }}>
-        <ListGroup variant="flush">
-          <ListGroup.Item>
-            <Card.Title style={{background: "white"}}>{tech.title.toUpperCase()}</Card.Title>
-            <Card.Text style={{background: "white"}}>{tech.description}</Card.Text>
-          </ListGroup.Item>
-          <ListGroup.Item>
-            <div style={{
-                width: "340px",
-                borderRadius: "40px",
-                background: "#ccc",
-                height: "40px"
-            }}>
-                <span
-                    style={{
-                        display: "block",
-                        height: "40px",
-                        textAlign: "right",
-                        background: color(tech.title),
-                        lineHeight: "40px",
-                        borderRadius: "40px",
-                        width: `${tech.percent}%`,
-                    }}
-                >{tech.percent}%</span>
-            </div>
-          </ListGroup.Item>
-        </ListGroup>
-        <br />
-        {/* tech 편집 버튼 */}
-        <div
-          style={{
-            display: "flex",
-            paddingLeft: "85px",
-            backgroundColor: "white",
-            marginLeft: "50px"
-          }}
-        >
-          {isEditable && (
-            <Button
-              variant="outline-info"
-              size="sm"
-              onClick={() => setIsEditing((prev) => !prev)}
-            >
-              편집
-            </Button>
-          )}
-          {/* tech 삭제 버튼 */}
-          {isEditable && (
-            <Button
-              variant="outline-secondary"
-              size="sm"
-              onClick={handleAlertShow}
-            >
-              삭제
-            </Button>
-          )}
-        </div>
-      </Card.Body>
-
-      <Modal
-        show={show}
-        onHide={handleAlertCancel}
-        style={{ background: "transparent" }}
-      >
-        <Modal.Header closeButton style={{ backgroundColor: "white" }}>
-          <Modal.Title style={{ backgroundColor: "white" }}>
-            삭제 확인
-          </Modal.Title>
-        </Modal.Header>
-        <Modal.Body style={{ backgroundColor: "white" }}>
-          정말로 삭제하시겠습니까?
-        </Modal.Body>
-        <Modal.Footer style={{ backgroundColor: "white" }}>
-          <Button variant="secondary" onClick={handleAlertCancel}>
-            취소
-          </Button>
-          <Button variant="danger" onClick={handleDelete}>
-            삭제
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    </>
-  );
-}
+			<Modal
+				show={show}
+				onHide={handleAlertCancel}
+				style={{ background: "transparent" }}
+			>
+				<Modal.Header closeButton style={{ backgroundColor: "white" }}>
+					<Modal.Title style={{ backgroundColor: "white" }}>
+						삭제 확인
+					</Modal.Title>
+				</Modal.Header>
+				<Modal.Body style={{ backgroundColor: "white" }}>
+					정말로 삭제하시겠습니까?
+				</Modal.Body>
+				<Modal.Footer style={{ backgroundColor: "white" }}>
+					<Button variant="secondary" onClick={handleAlertCancel}>
+						취소
+					</Button>
+					<Button variant="danger" onClick={handleDelete}>
+						삭제
+					</Button>
+				</Modal.Footer>
+			</Modal>
+		</>
+	);
+};
 
 export default TechCard;
